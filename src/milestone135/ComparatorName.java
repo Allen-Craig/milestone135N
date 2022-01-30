ffee7fbc8c29497f0ad1ed6330c7e5ae9f8d191d
Allen Craig <TheMightyCraig@gmail.com>
2022-01-30 11:38:18 -0700

    removed comment

--- src/milestone135/ComparatorName.java
+++ src/milestone135/ComparatorName.java
@@ -6,7 +6,6 @@ public class ComparatorName implements Comparator<BaseContact> {
 
 	@Override
 	public int compare(BaseContact o1, BaseContact o2) {
-		// TODO Auto-generated method stub
 		return o1.getName().compareTo(o2.getName());
 	}
 
